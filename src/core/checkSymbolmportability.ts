690c21989d0d4c39491cd0983e74101421f51bbe
m-shaka <buddha0818@gmail.com>
2023-08-15 16:01:23 +0900

    fix false positive and revert regex guard

--- src/core/checkSymbolmportability.ts
+++ src/core/checkSymbolmportability.ts
@@ -34,11 +34,11 @@ export function checkSymbolImportability(
     return;
   }
 
-  if (packageOptions.treatSelfReferenceAs === "external") {
+  if (packageOptions.treatSelfReferenceAs === "external" && possibleSubpathImportFromPackage.test(moduleSpecifier)) {
     // Check whether this import is the result of a self-reference.
     const lookupResult = lookupPackageJson(importerFilename);
     if (lookupResult !== null) {
-      if (moduleSpecifier.startsWith(lookupResult.packageJson.name)) {
+      if (checkIfImportIsSelfReference(moduleSpecifier, lookupResult.packageJson.name)) {
         // This is a self-reference, so treat as external.
         return;
       }
@@ -90,4 +90,12 @@ export function checkSymbolImportability(
     packageOptions,
   );
   return inPackage ? undefined : "package";
+}
+
+const possibleSubpathImportFromPackage = /^(?![./\\])([^/\\]*)(?:$|[/\\][^/\\])/;
+
+const checkIfImportIsSelfReference = (moduleSpecifier: string, packageName: string) => {
+  const importIsDefaultModule = moduleSpecifier === packageName
+  const importIsSubModule = moduleSpecifier.startsWith(`${packageName}/`)
+  return importIsDefaultModule || importIsSubModule
 }
