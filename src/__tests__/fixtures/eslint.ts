e6a9d3fddcc3866c886b36e2f71fc385ee323539
uhyo <uhyo@uhy.ooo>
2024-04-29 22:34:56 +0900

    test: add LegacyESLintTester and FlatESLintTester

--- src/__tests__/fixtures/eslint.ts
+++ src/__tests__/fixtures/eslint.ts
@@ -5,14 +5,75 @@ import path from "path";
 import { Program } from "typescript";
 import jsdocRule, { JSDocRuleOptions } from "../../rules/jsdoc";
 
-class ESLintTester {
+interface ESLintTester {
+  /**
+   * Lint file in the project (relative to project root).
+   */
+  lintFile(
+    filePath: string,
+    rules?: Partial<{ jsdoc: Partial<JSDocRuleOptions> }>,
+  ): Promise<TSESLint.Linter.LintMessage[]>;
+}
+
+class FlatESLintTester implements ESLintTester {
   #projectRoot: string;
   #linter: TSESLint.Linter;
   #program: Program;
-  constructor(projectRoot: string, flatConfig: boolean) {
+  constructor(projectRoot: string) {
     this.#projectRoot = projectRoot;
     this.#linter = new TSESLint.Linter({
       cwd: this.#projectRoot,
+      // @ts-expect-error FIXME: need support from typescript-eslint
+      configType: "flat",
+    });
+    this.#program = parser.createProgram("./tsconfig.json", projectRoot);
+  }
+  async lintFile(
+    filePath: string,
+    rules?: Partial<{ jsdoc: Partial<JSDocRuleOptions> }>,
+  ) {
+    const fileAbsolutePath = path.join(this.#projectRoot, filePath);
+    const code = await readFile(fileAbsolutePath, {
+      encoding: "utf8",
+    });
+
+    return this.#linter.verify(
+      code,
+      {
+        languageOptions: {
+          parser,
+          parserOptions: {
+            ecmaVersion: 2020,
+            tsconfigRootDir: this.#projectRoot,
+            project: "./tsconfig.json",
+            sourceType: "module",
+            program: this.#program,
+          },
+        },
+        plugins: {
+          // @ts-expect-error FIXME: need support from typescript-eslint
+          "import-access": { jsdoc: jsdocRule },
+        },
+        rules: {
+          "import-access/jsdoc": ["error", rules?.jsdoc ?? {}],
+        },
+      },
+      {
+        filename: fileAbsolutePath,
+      },
+    );
+  }
+}
+
+class LegacyESLintTester implements ESLintTester {
+  #projectRoot: string;
+  #linter: TSESLint.Linter;
+  #program: Program;
+  constructor(projectRoot: string) {
+    this.#projectRoot = projectRoot;
+    this.#linter = new TSESLint.Linter({
+      cwd: this.#projectRoot,
+      // @ts-expect-error FIXME: need support from typescript-eslint
       configType: "eslintrc",
     });
     this.#program = parser.createProgram("./tsconfig.json", projectRoot);
@@ -21,9 +82,6 @@ class ESLintTester {
 
     this.#linter.defineRule("import-access/jsdoc", jsdocRule);
   }
-  /**
-   * Lint file in the project (relative to project root).
-   */
   async lintFile(
     filePath: string,
     rules?: Partial<{ jsdoc: Partial<JSDocRuleOptions> }>,
@@ -55,12 +113,16 @@ class ESLintTester {
   }
 }
 
+const flatConfig = !!process.env.TEST_FLAT_CONFIG;
 let cache: ESLintTester | undefined;
 /**
  * get an ESLint instance for testing.
  */
 export function getESLintTester(): ESLintTester {
   const projectRoot = path.resolve(__dirname, "project");
-  const flatConfig = !!process.env.TEST_FLAT_CONFIG;
-  return (cache ||= new ESLintTester(projectRoot, flatConfig));
+  if (flatConfig) {
+    return (cache ||= new FlatESLintTester(projectRoot));
+  } else {
+    return (cache ||= new LegacyESLintTester(projectRoot));
+  }
 }
