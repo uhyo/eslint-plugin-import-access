ea83a4e57677ba5bf6d1eb32a157b3ece000d5b2
uhyo <uhyo@uhy.ooo>
2025-04-27 16:39:22 +0900

    chore: fix lint

--- eslint-local-rules.cjs
+++ eslint-local-rules.cjs
@@ -1,7 +1,7 @@
 // for eslint-plugin-local-rules
 try {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-  module.exports = require("./").rules;
+  module.exports = require("./dist/index.js").default.rules;
   // eslint-disable-next-line no-empty
 } catch (e) {
   console.warn(e);
